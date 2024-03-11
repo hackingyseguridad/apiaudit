cae3257b33ae66a60b741f24ec6e153e9227bf86
hackingyseguridad <antonio_taboada@hotmail.es>
2024-03-11 20:47:04 +0100

    Update apiaudit.sh

--- apiaudit.sh
+++ apiaudit.sh
@@ -40,7 +40,7 @@ curl -Is --http2-prior-knowledge $1 | head -1
 echo
 echo -e "\e[00;32m# Escaneo con Nmap de puertos web habituales ########################################################\e[00m" 
 echo
-nmap $1 -Pn -p80,81,443,4443,7443,8000,8080,8081,8443,8888 --script=http-enum --script=http-security-headers --script=http-methods --open -sCV -O
+nmap $1 -Pn -p80,81,443,4443,7443,8000,8080,8081,8443,8888,10443 --script=http-enum --script=http-security-headers --script=http-methods --open -sCV -O
 echo
 echo -e "\e[00;32m# Escaneo con Nmap de otros puertos  de servicio sensibles ########################################################\e[00m" 
 echo
