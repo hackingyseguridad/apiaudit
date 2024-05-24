aaeccdf12001454fb2c55981012725a3fb3b112d
hackingyseguridad <antonio_taboada@hotmail.es>
2024-05-24 09:18:02 +0200

    Update github0.sh

--- github0.sh
+++ github0.sh
@@ -1,6 +1,9 @@
 # Rest API para GitHub
 # Respuesta JSON
 #
+#
+curl -H "Authorization: token  ghp_ELylM8fMpaTPtw8zYqCCehpANtt5EN4Gy5BP" "https://api.github.com" 
+#
 curl -H "Authorization: token  ghp_ELylM8fMpaTPtw8zYqCCehpANtt5EN4Gy5BP" "https://api.github.com"   "current_user_url": "https://api.github.com/user"
 #
 #
