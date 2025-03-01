dd787c7d426675e90427478fcc8bd77ff016d232
hackingyseguridad <antonio_taboada@hotmail.es>
2025-03-01 21:28:08 +0100

    Create instalar.sh

---
+++ instalar.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+echo
+echo "Instalando ..."
+echo
+apt-get install nikto
+echo
+apt-get install nmap
+echo
+apt-get install dirb
+echo
+apt-get install gobuster
+echo
+apt-get install wapiti
+echo
+echo "... actualizando diccionarios ...  (R) 2025 hackingyseguridad.com "
+echo
+wget https://raw.githubusercontent.com/hackingyseguridad/diccionarios/refs/heads/master/ficheros.txt -q -O diccionario.txt  --inet4-only
+wc -l diccionario.txt
+echo ".."
+echo "..."
+wget https://raw.githubusercontent.com/hackingyseguridad/diccionarios/refs/heads/master/ficheros2.txt -q -O diccionario2.txt  --inet4-only
+wc -l diccionario2.txt
+echo "...."
+echo "....."
+echo
+echo "actualizado !!! "
